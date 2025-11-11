1388ee4f24e4e6e07a987781acd036e262390b3a
tanishq-007 <neela.t@northeastern.edu>
2025-11-10 19:01:09 -0500

    fixed

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -21,7 +21,7 @@ export default function KambazNavigation() {
             className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
             <ListGroupItem id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
                 action className="bg-black border-0 text-center">
-                <Image src="/images/NEU2.png" alt="" width="95" height="80" /></ListGroupItem>
+                <Image src="/images/NEU.png" alt="" width="95" height="80" /></ListGroupItem>
             <ListGroupItem as={Link} href="/Account"
                 className={`text-center border-0 bg-black
             ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
