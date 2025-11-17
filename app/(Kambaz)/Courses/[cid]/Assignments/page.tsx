09314c7b0e7a236d493dcaf41386b70f1eeb3db3
tanishq-007 <neela.t@northeastern.edu>
2025-11-17 11:34:09 -0500

    hi

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -61,7 +61,7 @@ export default function Assignments() {
                 {isFaculty && (
                     <div>
                         <button className="btn btn-secondary me-2">
-                            <FaPlus className="me-1" /> Group
+                            <FaPlus className="me-1" /> Groups
                         </button>
                         <button
                             className="btn btn-danger"
